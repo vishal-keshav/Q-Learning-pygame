1f722f4732129908c1cb1b9fa153842756733434
bulletcross <bulletcross@gmail.com>
2016-07-14 18:58:10 +0530

    game func complete

--- testing_main.py
+++ testing_main.py
@@ -1,5 +1,6 @@
 import os
 import pygame as pg
+import random
 from text_log import *
 """Utility functons"""
 def maxim(a,b):
@@ -14,6 +15,11 @@ def minim(a,b):
     else:
         return b
 
+"""Block crosses each other"""
+def crossover(x1,y1,x2,y2):
+    if (y1<y2+100) and (y1+100>y2) and (x1<x2+100) and (x1+100>x2):
+        return True
+    return False
 
 """Event handler"""
 def handle_event(event, pg, x, y):
@@ -61,9 +67,19 @@ clk = pg.time.Clock()
 """Objects on window"""
 path = os.path.join("resources", "ball1.jpg")
 ball_image = pg.image.load(path)
+path = os.path.join("resources", "ball2.jpg")
+negative_ball = pg.image.load(path)
+path = os.path.join("resources", "ball3.jpg")
+positive_ball = pg.image.load(path)
 ball_y = 0.7*disp_height
 ball_x = 0.45*disp_width
-
+nball_x = 0
+nball_y = 0
+draw_ball = 0
+##pball_x = 0
+##pball_y = 0
+points = 0
+bool_draw = True
 """Game loop"""
 abort = False
 while not abort:
@@ -77,11 +93,34 @@ while not abort:
         
     #update the game window, set fps
     disp.fill(bg_color)
-    log = "pos_x: " + str(ball_x) + ", pos_y: " + str(ball_y) 
+    #log = "pos_x: " + str(ball_x) + ", pos_y: " + str(ball_y)
+    log = "points: " + str(points)
     display_msg(log, pg, disp)
+    
+    if nball_y > 600:
+        nball_y = -5
+        nball_x = random.randrange(0,500)
+        draw_ball = random.randrange(0,2)
+        bool_draw = True
+    
+    nball_y = nball_y + 20
     draw(ball_image,disp,ball_x,ball_y)
+    if crossover(ball_x, ball_y, nball_x, nball_y):
+        if draw_ball <=0 and bool_draw:
+            points = points - 1
+            #print(points)
+        elif draw_ball >0 and bool_draw:
+            points = points + 1
+            #print(points)
+        bool_draw = False
+        
+    elif draw_ball <= 0 and bool_draw:
+        draw(negative_ball,disp,nball_x,nball_y)
+    elif draw_ball > 0 and bool_draw:
+        draw(positive_ball,disp,nball_x,nball_y)
+    
     pg.display.update()
-    clk.tick(30)
+    clk.tick(45)
 
 """ We have come out of game logic loop"""
 pg.quit()
